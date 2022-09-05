91a3852d3a5cda4de7f686cc5f04c66863243289
Lucas Nicolas Alberca <86059300+Capigol@users.noreply.github.com>
2022-09-05 11:28:50 -0300

    Update drogabilidad_streamlit.py

--- drogabilidad_streamlit.py
+++ drogabilidad_streamlit.py
@@ -1,3 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 10 11:12:49 2022
+
+@author: adm
+"""
+
+
+# Introduction
+
 
 # Needed packages
 
@@ -6,7 +16,6 @@ import pandas as pd
 from pandas import DataFrame
 from Bio import SeqIO
 import numpy as np
-
 import streamlit as st
 from pathlib import Path
 import base64
@@ -20,8 +29,7 @@ import io
 #---------------------------------#
 # Page layout
 ## Page expands to full width
-st.set_page_config(page_title='LIDEB Tools - Druggability',
-    layout='wide')
+st.set_page_config(page_title='LIDeB Tools - Druggability', page_icon="📏", layout='wide')
 
 ######
 # Function to put a picture as header   
@@ -30,25 +38,31 @@ def img_to_bytes(img_path):
     encoded = base64.b64encode(img_bytes).decode()
     return encoded
 
-
 image = Image.open('cropped-header.png')
 st.image(image)
-
-st.markdown("![Twitter Follow](https://img.shields.io/twitter/follow/LIDeB_UNLP?style=social)")
+#####
+st.write("[![Website](https://img.shields.io/badge/website-LIDeB-blue)](https://lideb.biol.unlp.edu.ar)[![Twitter Follow](https://img.shields.io/twitter/follow/LIDeB_UNLP?style=social)](https://twitter.com/intent/follow?screen_name=LIDeB_UNLP)")
+st.markdown("###### Made in 🐍 and [![this is an image link](https://i.imgur.com/iIOA6kU.png)](https://www.streamlit.io/) with :heart: by [LIDeB](https://twitter.com/LIDeB_UNLP)")
+#####
 st.subheader(":pushpin:" "About Us")
 st.markdown("We are a drug discovery team with an interest in the development of publicly available open-source customizable cheminformatics tools to be used in computer-assisted drug discovery. We belong to the Laboratory of Bioactive Research and Development (LIDeB) of the National University of La Plata (UNLP), Argentina. Our research group is focused on computer-guided drug repurposing and rational discovery of new drug candidates to treat epilepsy and neglected tropical diseases.")
-st.markdown(":computer:""**Web Site** " "<https://lideb.biol.unlp.edu.ar>")
 
 
 # Introduction
 #---------------------------------#
 
 st.write("""
-# LIDeB Tools
+# LIDeB Tools - Druggability
 
 **It is a free web-application for Druggability Prediction**
 
-The tool uses the following packages [RDKIT](https://www.rdkit.org/docs/index.html), [PyBioMed](https://github.com/gadsbyfly/PyBioMed/), [Biopython](https://biopython.org/), [Plotly](https://plotly.com/python/)
+In the framework target-guided drug discovery it is important to be able to assess the druggability of
+the proposed drug target prior to the implementation of a drug discovery project. Druggability is a
+concept coined by Hopkins and Groom to refer to the ability of a protein to be modulated by small,
+drug-like molecules.
+This tool generates four predictions of druggability based on protein sequence. 
+
+The tool uses the following packages: [PyBioMed](https://github.com/gadsbyfly/PyBioMed/), [Biopython](https://biopython.org/), [Seaborn](https://seaborn.pydata.org/index.html)
 
 The next workflow summarizes the steps performed by this method:
     
@@ -56,8 +70,8 @@ The next workflow summarizes the steps performed by this method:
 """)
 
 
-# image = Image.open('clustering_metodo_manu.png')
-# st.image(image, caption='Clustering Workflow')
+image = Image.open('workflow_druggability.png')
+st.image(image, caption='Druggability Workflow')
 
 
 ########### OPTIONS #######
@@ -68,8 +82,10 @@ st.sidebar.header('Upload your fasta')
 
 archivo = st.sidebar.file_uploader("Upload a fasta file with sequences", type=["fasta"])
 
+applicability_domain_h = st.sidebar.slider('h* for applicability domain', 2, 3, 3, 1)
 
-##########################################DE ACÁ PARA ABAJO NO HAY QUE TOCAR NADA################################################
+
+# Druggability
 
 def druggability_app(archivo):
     seq=[]
@@ -146,7 +162,7 @@ def druggability_app(archivo):
     
     corte=0.5
     scores=(M1,M2,M3,M4)
-    		
+    # st.write(scores)
     predicciones=[] #Lista de listas, cada elemento de la lista es uno de los modelos. Cada uno de los modelos tiene las train predicciones binarizadas.
     for models in scores:
       for j in models:#Transformo a valor de clase a cada una de las predicciones de los modelos.
@@ -157,7 +173,6 @@ def druggability_app(archivo):
           else: 
             j='Druggable'
             predicciones.append(j)	
-    
     
     predicciones=[predicciones[j:j+len(valores.index)] for j in range (0,len(predicciones),len(valores.index))]#Separo la lista original en (n°modelos) listas
     
@@ -191,22 +206,21 @@ def druggability_app(archivo):
     		z_=np.matmul(y_,xt_)
     		hi=z_
     		h.append(hi)
-    h2=10/156 # 2k/n
-    h3=15/156 # 3k/n
+    if applicability_domain_h == 2:
+        h2=10/156 # 2k/n
+    else:
+        h2=15/156 # 3k/n
     
     dominio=[]
     
     for g in h:
-    	if g<h2:
-    		w='YES**'
-    		dominio.append(w)
-    	else:
-    		if g<h3:
-    			w='YES*'
-    			dominio.append(w)
-    		else:
-    			w='NO'
-    			dominio.append(w)
+        if g<h2:
+            w='YES'
+            dominio.append(w)
+        else:
+            w='NO'
+            dominio.append(w)
+
     
     dominio=[dominio[j:j+len(valores.index)] for j in range (0,len(predicciones*len(valores.index)),len(valores.index))]#Separo la lista original en (n°modelos) listas
     v=0
@@ -217,60 +231,156 @@ def druggability_app(archivo):
     resultados_final = pd.concat([resultados,dominio],axis=1)
     resultados_final['name'] = seq
     columnas=["Protein_ID","PREDICT_1","DA_1","PREDICT_2","DA_2","PREDICT_3","DA_3","PREDICT_4","DA_4"]
-    resultados_final = resultados_final[columnas]
-    st.write(resultados_final)
+    resultados_final_crudo = resultados_final[columnas]
     
-    return resultados_final
+    scores1 = pd.DataFrame(scores).T
+    
+    return resultados_final_crudo, scores1
 
-def druggability_heatmap(resultados_final):
-    resultados_final['-'] = 5
-    resultados_final['--'] = 5
-    resultados_final['---'] = 5
-    resultados_final['----'] = 5
 
-    cols = ["Protein_ID","-","PREDICT_1","DA_1","--","PREDICT_2","DA_2","---","PREDICT_3","DA_3","----","PREDICT_4","DA_4"]
-    resultados_final = resultados_final[cols]
-    resultados_final.replace("Non-druggable","0",inplace=True)
-    resultados_final1 = resultados_final.replace("Druggable","10")
-    resultados_final1 = resultados_final1.replace("YES*","7")
-    resultados_final1 = resultados_final1.replace("YES**","10")
-    resultados_final1 = resultados_final1.replace("NO","0")
+#%%
+
+
+def druggability_analisis(resultados_final):
+    resultados_final1 = resultados_final.copy()
+    
+    st.markdown(str(resultados_final1.shape[0]) + " proteins were loaded")
+    
     resultados_final1.set_index('Protein_ID',inplace=True)
-    resultados_final = resultados_final1.apply(pd.to_numeric, errors = 'coerce')
-    import seaborn as sns
-    import matplotlib.pylab as plt
     
-    # st.write(resultados_final)
-    cmap = sns.diverging_palette(20,130,center='light',as_cmap=True,s=100,l=50,sep=100)
-    # cmap = sns.diverging_palette(10,230,sep=1,center='light',n=4)
+    models = [["PREDICT_1","DA_1"],["PREDICT_2","DA_2"],["PREDICT_3","DA_3"],["PREDICT_4","DA_4"]]
     
-    # plt.figure(figsize=(45,4))
-    graph = sns.heatmap(resultados_final[0:], annot=False, square=True, linewidths=1.0, cmap=cmap, fmt=".1f",robust=True,
-                        annot_kws={'fontsize':5}, cbar_kws={"orientation": "vertical"}, cbar=False, vmin=0, vmax=10)
-    # bottom, top = graph.get_ylim()
-    # graph.set_ylim(bottom, top)
+    data_4_4 = resultados_final1.copy()
     
-    # here set the labelsize by 20
-    
-    plt.yticks(fontsize=5, rotation=0);plt.xticks(fontsize=5, rotation=90)
-    plt.ylabel('Targets',fontsize=5);plt.xlabel("",fontsize=5)
-    
-    # cbar = graph.collections[0].colorbar
-    
-    # cbar.ax.tick_params(labelsize=10)
-    
-    plt.tight_layout()
-    st.pyplot(plt)
+    for index, row in data_4_4.iterrows():
+        druggables_da = []
+        druggables_no_da = []
+        non_druggables_da = []
+        non_druggables_no_da = []
 
+        for model in models:
+            pred = row[model[0]]
+            da = row[model[1]]
+            if pred == "Druggable" and da == "YES":
+                druggables_da.append(1)
+            if pred == "Druggable" and da == "NO":
+                druggables_no_da.append(1)  
+            if pred == "Non-druggable" and da == "YES":
+                non_druggables_da.append(1)
+            if pred == "Non-druggable" and da == "NO":
+                 non_druggables_no_da.append(1)               
+            else:
+                pass
+            
+        # Si al menos un modelo la predice "NO Drogable" y entra en DA, queda "No druggable"
+        if len(non_druggables_da) >= 1:
+            data_4_4.loc[index,'CLASSIFICATION'] = "Non-Druggable"  
+        
+        # Si hay 3 o más modelos que dan fuera del DA, da "Non-conclusive"
+        elif (len(druggables_no_da) + len(non_druggables_no_da)) >= 3:
+            data_4_4.loc[index,'CLASSIFICATION'] = "Non-Conclusive"  
+        
+        # Si las 4 dan drogables y está en da, se considera drogable.
+        elif len(druggables_da) == 4:
+            data_4_4.loc[index,'CLASSIFICATION'] = "Druggable"
+        
+        # si 2 dan drogables en DA, y dos no drogables fuera del DA, se considera drogable.
+        elif len(druggables_da) == 2 and len(non_druggables_no_da) == 2:
+            data_4_4.loc[index,'CLASSIFICATION'] = "Druggable"
+        else:
+            data_4_4.loc[index,'CLASSIFICATION'] = "Druggable"
+        
+    all_druggable = data_4_4.loc[data_4_4['CLASSIFICATION'] == "Druggable"]
+    all_no_druggable = data_4_4.loc[data_4_4['CLASSIFICATION'] == "Non-Druggable"]
+    inconclusive_all = data_4_4.loc[data_4_4['CLASSIFICATION'] == "Non-Conclusive"]
+
+    data_pie_total = [all_druggable.shape[0],all_no_druggable.shape[0],inconclusive_all.shape[0]]
+
+
+    keys =["Druggable", "Non-Druggable","Non-Conclusive"]
+    
+    # Create plot:
+
+    import plotly.graph_objects as go
+    
+    fig = go.Figure(data=[go.Pie(labels=keys, values=data_pie_total)])
+    
+    fig.update_layout(plot_bgcolor = 'rgb(256,256,256)',
+                            title_font = dict(size=25, family='Calibri', color='black'),
+                            font =dict(size=20, family='Calibri'),
+                            legend_title_font = dict(size=18, family='Calibri', color='black'),
+                            legend_font = dict(size=15, family='Calibri', color='black'))
+
+    st.plotly_chart(fig)
+    return data_4_4
+
+#%%
+def filedownload(df):
+    csv = df.to_csv(index=True,header=True)
+    b64 = base64.b64encode(csv.encode()).decode()  # strings <-> bytes conversions
+    href = f'<a href="data:file/csv;base64,{b64}" download="Druggability_predictions.csv">Download CSV File with the druggability predictions</a>'
+    return href
+
+#%%
 
 if archivo is not None:
     run = st.sidebar.button("PREDICT")
     if run == True:
         file_name_st = archivo.name
         archivo_ok = io.TextIOWrapper(archivo)
-        resultados_final = druggability_app(archivo_ok)
-        druggability_heatmap(resultados_final)
+        resultados_final_crudo, scores1 = druggability_app(archivo_ok)
+        all_to_write = druggability_analisis(resultados_final_crudo)        
+        
+        st.markdown(":point_down: **Here you can download the raw predictions for 3/4 models**", unsafe_allow_html=True)
+        st.markdown(filedownload(all_to_write), unsafe_allow_html=True)
+
+
+# Example file
+else:
+    st.info('Awaiting for FASTA file to be uploaded.')
+    if st.button('Press to use Example Dataset'):
+        archivo = open("multi.fasta","rb")
+        file_name_st = archivo.name
+        archivo_ok = io.TextIOWrapper(archivo)
+        resultados_final_crudo, scores1 = druggability_app(archivo_ok)
+        all_to_write = druggability_analisis(resultados_final_crudo)        
+        st.markdown(":point_down: **Here you can download the raw predictions for 3/4 models**", unsafe_allow_html=True)
+        st.markdown(filedownload(all_to_write), unsafe_allow_html=True)
+
 
 st.markdown("""
          **To cite the application, please reference XXXXXXXXX**
          """)
+
+
+
+
+#Footer edit
+
+footer="""<style>
+a:link , a:visited{
+color: blue;
+background-color: transparent;
+text-decoration: underline;
+}
+a:hover,  a:active {
+color: red;
+background-color: transparent;
+text-decoration: underline;
+}
+.footer {
+position: fixed;
+left: 0;
+bottom: 0;
+width: 100%;
+background-color: white;
+color: black;
+text-align: center;
+}
+</style>
+<div class="footer">
+<p>Made in  🐍 and <img style='display: ; ' href="https://streamlit.io" src="https://i.imgur.com/iIOA6kU.png" target="_blank"></img> Developed with ❤️ by <a style='display:; text-align: center;' href="https://lideb.biol.unlp.edu.ar/" target="_blank">LIDeB</a></p>
+</div>
+"""
+st.markdown(footer,unsafe_allow_html=True)
+
